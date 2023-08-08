7f5b2d5b27f655e276b057e9ef289e4360819d7f
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-08-08 23:36:44 +0200

    update sdk

--- src/main/java/org/fusioproject/sample/Main.java
+++ src/main/java/org/fusioproject/sample/Main.java
@@ -1,32 +1,28 @@
 package org.fusioproject.sample;
 
+import app.sdkgen.client.Exception.ClientException;
 import app.sdkgen.client.TokenStore.MemoryTokenStore;
 import app.sdkgen.client.TokenStoreInterface;
 import org.fusioproject.sdk.Client;
-import org.fusioproject.sdk.backend.BackendRoutesResource;
-import org.fusioproject.sdk.backend.Collection_Category_Query;
-import org.fusioproject.sdk.backend.Route_Collection;
+import org.fusioproject.sdk.backend.OperationCollection;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws URISyntaxException, IOException {
+    public static void main(String[] args) throws ClientException {
         List<String> scopes = new ArrayList<>();
         scopes.add("backend");
         TokenStoreInterface tokenStore = new MemoryTokenStore();
 
         Client client = new Client("https://demo.fusio-project.org/", "test", "FRsNh1zKCXlB", scopes, tokenStore);
 
-        BackendRoutesResource resource = client.backend().backendRoute().getBackendRoutes();
-        Route_Collection collection = resource.backendActionRouteGetAll(new Collection_Category_Query());
+        OperationCollection operations = client.backend().operation().getAll(0, 16, "");
 
         System.out.println("Routes:");
-        for (int i = 0; i < collection.getEntry().length; i++) {
-            System.out.println("* " + collection.getEntry()[i].getPath());
+        for (int i = 0; i < operations.getEntry().length; i++) {
+            System.out.println("* " + operations.getEntry()[i].getHttpPath());
         }
     }
 
